151bf358f3eae47988c4b8c21941399bb0778fe9
yukikitayama <yk2797@columbia.edu>
2024-06-21 20:57:49 -0700

    union, not in

---
+++ mock-coding-interview/toshiya/toshiya_1965_employees_with_missing_information.sql
@@ -0,0 +1,21 @@
+-- Write your PostgreSQL query statement below
+select
+  employee_id
+from
+  employees
+where
+  employee_id not in (
+    select employee_id from salaries
+  )
+union
+select
+  employee_id
+from
+  salaries
+where
+  employee_id not in (
+    select employee_id from employees
+  )
+order by
+  1
+;
