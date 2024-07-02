7dd114dcb2b5955da5b529f1fedebd33889180d7
yukikitayama <yk2797@columbia.edu>
2024-07-02 15:29:20 -0700

    ifnull

---
+++ mock-coding-interview/satoshi/satoshi_1251_average_selling_price.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+select
+  a.product_id,
+  round(ifnull(sum(a.price * b.units) / sum(b.units), 0), 2) as average_price
+from
+  prices as a
+left join
+  unitssold as b
+on
+  a.product_id = b.product_id
+  and b.purchase_date between a.start_date and a.end_date
+group by
+  1
+;
