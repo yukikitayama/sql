4e35aefcfd0604171ed520f9f18eceb8f8a8f0b0
yukikitayama <yk2797@columbia.edu>
2024-08-25 21:52:50 -0700

    rank

---
+++ mock-coding-interview/satoshi/satoshi_3246_premier_league_table_ranking.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select
+  team_id,
+  team_name,
+  wins * 3 + draws as points,
+  rank() over(order by wins * 3 + draws desc) as position
+from
+  teamstats
+order by
+  3 desc,
+  2
+;
